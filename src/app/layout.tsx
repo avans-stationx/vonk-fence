1550917449d69b4ec8425aa4c92b401990340dc7
Niels van de Pas <ik@nielsvandepas.nl>
2024-03-26 20:25:40 +0100

    Use new audio system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import AudioPlayer from './components/AudioPlayer';
+import AudioSystemProvider from './components/AudioSystem/AudioSystemProvider';
+import BackgroundAudio from './components/AudioSystem/BackgroundAudio';
+import RandomSoundPlayer from './components/AudioSystem/RandomSoundPlayer';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,8 +20,31 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AudioPlayer src="/ambient.mp3" repeat={true} />
-        {children}
+        <AudioSystemProvider>
+          {children}
+          <BackgroundAudio
+            src="/audio/00_Ambient_MONO.wav"
+            channel="left"
+            play={true}
+            loop={true}
+          />
+          <RandomSoundPlayer
+            sounds={[
+              '/audio/01_Gluur_MONO.wav',
+              '/audio/02_Gluur_MONO.wav',
+              '/audio/03_Gluur_MONO.wav',
+              '/audio/04_Gluur_MONO.wav',
+              '/audio/05_Gluur_MONO.wav',
+              '/audio/06_Gluur_MONO.wav',
+              '/audio/07_Gluur_MONO.wav',
+              '/audio/08_Gluur_MONO.wav',
+              '/audio/09_Gluur_MONO.wav',
+            ]}
+            channel="right"
+            intervalMillis={10000}
+            interruptGroup="ralf"
+          />
+        </AudioSystemProvider>
       </body>
     </html>
   );
